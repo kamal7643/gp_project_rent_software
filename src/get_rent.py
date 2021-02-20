e6db12fdee23954994df27ffcd8a65febc19d186
kamal7643 <kamalswami0808@gmail.com>
2021-02-20 12:15:10 +0530

    names

--- src/get_rent.py
+++ src/get_rent.py
@@ -2,7 +2,7 @@ import pandas as pd
 import tkinter as tk
 
 def get_rent(root):
-    print("list")
-    data=pd.read_excel(r'C:\Users\kamal swami\PycharmProjects\pythonProject\cache\yesorno.xlsx')
+    data=pd.read_excel(r'C:\Users\kamal swami\PycharmProjects\pythonProject\cache\general.xlsx')
     data=pd.DataFrame(data,columns=['model','nonAC','AC'])
+
     print(data)
